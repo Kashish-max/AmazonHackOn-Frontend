32e4fc345514f2b70e9e358cfb6382cb946f6a3d
Manav Madhu <manavmadhu397@gmail.com>
2022-07-30 00:47:08 +0530

    added password authentication

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -13,20 +13,22 @@ class Login extends Component {
             {/* <div className="fadeIn first mb-3">
               <h1>Login</h1>
             </div> */}
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={this.handleSubmit} className="login-form-wrapper">
+              <div>
               <input
                 type="text"
                 id="login"
-                className="fadeIn second mb-3 username-input-text"
+                className="fadeIn second mb-3"
                 name="username"
                 placeholder="Username"
               />
+              </div>
               <input
                 type="password"
                 id="password"
                 className="fadeIn third mb-3"
                 name="password"
-                placeholder="password"
+                placeholder="Password"
               /><div className="submit-btn">
               <input type="submit" className="fadeIn fourth" value="Log In" />
               </div>
